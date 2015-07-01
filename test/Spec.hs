cf529be65f1870bc7688db01029bb22bfb98f097
Ilya Smelkov <triplepointfive@gmail.com>
2015-07-01 22:18:52 +0300

    Add importer tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,13 +1,29 @@
 module Main where
 
+import Data.Either
+
 import Test.Hspec
 
 import Codec.Soten.BaseImporter
+import Codec.Soten.Importer
 import Codec.Soten.Importer.StlImporter
 import Codec.Soten.Util
 
 main :: IO ()
 main = hspec $ do
+  describe "Importer" $ do
+    context "Errors" $ do
+      it "Unknown format" $ do
+        eitherScene <- readModelFile "soten.cabal"
+        (isLeft eitherScene) `shouldBe` True
+      it "Non exist file" $ do
+        eitherScene <- readModelFile "bla-bla-bla"
+        (isLeft eitherScene) `shouldBe` True
+    context "Loads formats" $ do
+      it "STL" $ do
+        eitherScene <- readModelFile "models/stl/block_ascii.stl"
+        (isRight eitherScene) `shouldBe` True
+
   describe "Importers" $ do
     context "STL" $ do
       it "checks extension of ascii file" $ do
@@ -22,4 +38,3 @@ main = hspec $ do
       it "checks header of binary file" $ do
         status <- canImport StlImporter "models/stl/block_bin.stl" CheckHeader
         status `shouldBe` True
-
