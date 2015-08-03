cb8192d8924df226e03b5ac0d13fb7b154b518c5
Ilya Smelkov <triplepointfive@gmail.com>
2015-08-03 21:21:45 +0300

    Fix infacing test

--- test/Codec/Soten/PostProcess/FixInfacingNormalsTest.hs
+++ test/Codec/Soten/PostProcess/FixInfacingNormalsTest.hs
@@ -11,19 +11,19 @@ import Codec.Soten.PostProcess.FixInfacingNormals
 import Codec.Soten.Scene
 import Codec.Soten.Scene.Mesh
 
-vertices = V.fromList [V3 1 0 0, V3 0 1 0, V3 0 0 1]
-normals  = V.replicate 3 (V3 1 1 1)
-face     = Face (V.fromList [0, 1, 2])
+vertices = V.fromList [V3 1 0 0, V3 0 1 0, V3 0 0 1, V3 (-1) 0 0, V3 0 (-1) 0, V3 0 0 (-1)]
+normals  = (V.replicate 3 (V3 1 1 1)) V.++ (V.replicate 3 (V3 (-1) (-1) (-1)))
+faces    = V.fromList [Face (V.fromList [0, 1, 2]), Face (V.fromList [3, 4, 5])]
 
 validMesh = newMesh
     & meshVertices .~ vertices
     & meshNormals  .~ normals
-    & meshFaces    .~ V.singleton face
+    & meshFaces    .~ faces
 
 invalidMesh = newMesh
     & meshVertices .~ vertices
     & meshNormals  .~ V.map (*(-1)) normals
-    & meshFaces    .~ V.singleton face
+    & meshFaces    .~ faces
 
 fixInfacingNormalsTest :: Spec
 fixInfacingNormalsTest = do
@@ -32,14 +32,14 @@ fixInfacingNormalsTest = do
       let scene = newScene & sceneMeshes  .~ V.singleton validMesh
           fixedSceneMesh = V.head $ (apply scene) ^. sceneMeshes
       it "Left normals unchanged" $ do
-         (fixedSceneMesh ^. meshNormals) `shouldBe` normals
+        (fixedSceneMesh ^. meshNormals) `shouldBe` normals
       it "Left faces unchanged" $ do
-         (fixedSceneMesh ^. meshFaces) `shouldBe` V.singleton face
+        (fixedSceneMesh ^. meshFaces) `shouldBe` faces
     context "Invalid mesh" $ do
       let scene = newScene & sceneMeshes  .~ V.singleton invalidMesh
           fixedSceneMesh = V.head $ (apply scene) ^. sceneMeshes
       it "Inverts normals" $ do
-         (fixedSceneMesh ^. meshNormals) `shouldBe` normals
+        (fixedSceneMesh ^. meshNormals) `shouldBe` normals
       it "Flips faces" $ do
-         (fixedSceneMesh ^. meshFaces) `shouldBe` V.singleton (Face (V.fromList [2, 1, 0]))
-
+        (fixedSceneMesh ^. meshFaces) `shouldBe`
+          (V.fromList [Face (V.fromList [2, 1, 0]), Face (V.fromList [5, 4, 3])])
